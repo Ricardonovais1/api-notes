f4899468ef88cc15d01bb38bbbb1ca4659701f64
Ricardonovais1 <ricardonovais1@gmail.com>
2023-08-23 22:56:39 -0300

    whitelist frontend no netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://bright-basbousa-49518b.netlify.app/"
+    origins "*"
 
     resource "*",
       headers: :any,
